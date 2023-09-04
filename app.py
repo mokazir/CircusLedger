13e7fe553cafb709098e13e398db7fb54e520b13
Mohamed Kasir K <kasirk@pm.me>
2023-09-04 14:52:40 +0530

    Add DC logo and DC logo with CL text SVGs. Update layout background to #eeeeee. Add Company register template. Update layout template. Update Login template. Add register template. Add login, register and half of compregister routes in app.py. Update USD to INR in helper.py.

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required, inr  # validate_phno  # lookup
 
 # Configure application
 app = Flask(__name__)
 
 # Custom filter
-app.jinja_env.filters["usd"] = usd
+app.jinja_env.filters["inr"] = inr
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -19,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///circusledger.db")
 
 
 @app.after_request
@@ -61,7 +61,6 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -71,14 +70,19 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        loguser = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(loguser) != 1 or not check_password_hash(
+            loguser[0]["hash"], request.form.get("password")
+        ):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = loguser[0]["id"]
+        session["company_id"] = loguser[0]["company_id"]
 
         # Redirect user to home page
         return redirect("/")
@@ -109,7 +113,186 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        regusername = request.form.get("username")
+        if not regusername:
+            return apology("Username not provided")
+
+        # Query database for username
+        reguser = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Check username exists
+        if len(reguser) == 1:
+            return apology("Username already exsists")
+
+        # Ensure password was submitted
+        regemail = request.form.get("email")
+        if not regemail:
+            return apology("must provide Email Address")
+
+        # Ensure password was submitted
+        regpassword = request.form.get("password")
+        if not regpassword:
+            return apology("must provide password")
+
+        # Ensure confirmation was submitted
+        regconfirm = request.form.get("confirmation")
+        if not regconfirm:
+            return apology("must provide password (again)")
+
+        if not regpassword == regconfirm:
+            return apology("password must be same")
+
+        regid = db.execute(
+            "INSERT INTO users (username, email, hash, phno, type) VALUES(?, ?, ?, ?, ?)",
+            regusername,
+            regemail,
+            generate_password_hash(regpassword),
+            request.form.get("phno"),
+            "admin",
+        )
+
+        # Remember which user has logged in
+        session["user_id"] = regid
+
+        # Redirect user to home page
+        flash("User successfully registered")
+        return redirect("/compregister")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+@app.route("/compregister", methods=["GET", "POST"])
+def compregister():
+    """Register company"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        compreg = {}
+
+        # NOT NULL fields
+        # Ensure company name was submitted
+        compreg["name"] = request.form.get("name")
+        if not compreg["name"]:
+            return apology("Company name not provided")
+
+        # Ensure Address:Building number was submitted
+        compreg["addrBno"] = request.form.get("addrBno")
+        if not compreg["addrBno"]:
+            return apology("Address: Building number not provided")
+
+        # Ensure Address:Street name was submitted
+        compreg["addrSt"] = request.form.get("addrSt")
+        if not compreg["addrSt"]:
+            return apology("Address: Street name not provided")
+
+        # Ensure Address:Area/Town was submitted
+        compreg["addrLoc"] = request.form.get("addrLoc")
+        if not compreg["addrLoc"]:
+            return apology("Address: Area/Town not provided")
+
+        # Ensure Address:City/District was submitted
+        compreg["addrDist"] = request.form.get("addrDist")
+        if not compreg["addrDist"]:
+            return apology("Address: City/District not provided")
+
+        # Ensure Address:State was submitted
+        compreg["addrState"] = request.form.get("addrState")
+        if not compreg["addrState"]:
+            return apology("Address: State not provided")
+
+        # Ensure Address:Pincode was submitted
+        compreg["addrPncd"] = request.form.get("addrPncd")
+        if not compreg["addrPncd"]:
+            return apology("Address: Pincode not provided")
+
+        # Ensure Mobile number was submitted
+        compreg["phno1"] = request.form.get("phno1")
+        if not compreg["phno1"]:
+            return apology("Mobile number not provided")
+
+        # Ensure GSTIN was submitted
+        compreg["gstin"] = request.form.get("gstin")
+        if not compreg["gstin"]:
+            return apology("GSTIN not provided")
+
+        # Can be NULL fields
+        # Collect all the optional fields
+        compreg["addrBnm"] = request.form.get("addrBnm")
+        compreg["Flno"] = request.form.get("Flno")
+        compreg["phno2"] = request.form.get("phno2")
+        compreg["email"] = request.form.get("email")
+        compreg["website"] = request.form.get("website")
+        compreg["bnkAcnm"] = request.form.get("bnkAcnm")
+        compreg["bnkAcno"] = request.form.get("bnkAcno")
+        compreg["bnkNm"] = request.form.get("bnkNm")
+        compreg["bnkIfsc"] = request.form.get("bnkIfsc")
+
+        # UNIQUE fields
+
+        # Check for a valid mobile number
+        valid = validate_phno(compreg["phno1"])
+        if not valid:
+            return apology("Invalid mobile number")
+
+        # Done until here
+
+        # Query database for phno1
+        compregphno1 = db.execute(
+            "SELECT * FROM companies WHERE phno1 = ?", request.form.get("phno1")
+        )
+
+        # Check username exists
+        if len(compregphno1) == 1:
+            return apology("Username already exsists")
+
+        # Ensure password was submitted
+        regemail = request.form.get("email")
+        if not regemail:
+            return apology("must provide Email Address")
+
+        # Ensure password was submitted
+        regpassword = request.form.get("password")
+        if not regpassword:
+            return apology("must provide password")
+
+        # Ensure confirmation was submitted
+        regconfirm = request.form.get("confirmation")
+        if not regconfirm:
+            return apology("must provide password (again)")
+
+        if not regpassword == regconfirm:
+            return apology("password must be same")
+
+        regid = db.execute(
+            "INSERT INTO users (username, email, hash, phno, type) VALUES(?, ?, ?, ?, ?)",
+            regusername,
+            regemail,
+            generate_password_hash(regpassword),
+            request.form.get("phno"),
+            "admin",
+        )
+
+        # Remember which user has logged in
+        session["user_id"] = regid
+
+        # Redirect user to home page
+        flash("User successfully registered")
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("compregister.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
--- helpers.py
+++ helpers.py
@@ -1,10 +1,10 @@
-import csv
-import datetime
-import pytz
-import requests
-import subprocess
-import urllib
-import uuid
+# import csv
+# import datetime
+# import pytz
+# import requests
+# import subprocess
+# import urllib
+# import uuid
 
 from flask import redirect, render_template, session
 from functools import wraps
@@ -12,16 +12,26 @@ from functools import wraps
 
 def apology(message, code=400):
     """Render message as an apology to user."""
+
     def escape(s):
         """
         Escape special characters.
 
         https://github.com/jacebrowning/memegen#special-characters
         """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
             s = s.replace(old, new)
         return s
+
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
@@ -31,16 +41,21 @@ def login_required(f):
 
     http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
     """
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
+        if session.get("company_id") is None:
+            return redirect("/comregister")
         return f(*args, **kwargs)
+
     return decorated_function
 
 
+"""
 def lookup(symbol):
-    """Look up quote for symbol."""
+    # Look up quote for symbol.
 
     # Prepare API request
     symbol = symbol.upper()
@@ -57,22 +72,30 @@ def lookup(symbol):
 
     # Query API
     try:
-        response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
+        response = requests.get(
+            url,
+            cookies={"session": str(uuid.uuid4())},
+            headers={"User-Agent": "python-requests", "Accept": "*/*"},
+        )
         response.raise_for_status()
 
         # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
         quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
         quotes.reverse()
         price = round(float(quotes[0]["Adj Close"]), 2)
-        return {
-            "name": symbol,
-            "price": price,
-            "symbol": symbol
-        }
+        return {"name": symbol, "price": price, "symbol": symbol}
     except (requests.RequestException, ValueError, KeyError, IndexError):
         return None
+"""
 
 
-def usd(value):
-    """Format value as USD."""
-    return f"${value:,.2f}"
+def inr(value):
+    """Format value as INR."""
+    return f"₹{value:,.2f}"
+
+
+# Function to check a string of phone number for its length to be exactly 10 and it contain strictly numbers from 0-9 only
+
+
+
+# Function to check a string of email to be valid with regular expressions
